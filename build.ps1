f103d1d3afc97f3d5c94da97bcbc9d051f48c163
DavidOConnor <david.alan.oconnor@gmail.com>
2019-01-20 09:52:51 -0500

    update

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
 cargo build --target wasm32-unknown-unknown
-wasm-bindgen ../../target/wasm32-unknown-unknown/debug/seed_homepage.wasm --no-modules --out-dir ./pkg --out-name package
+wasm-bindgen target/wasm32-unknown-unknown/debug/seed_homepage.wasm --no-modules --out-dir ./pkg --out-name package
